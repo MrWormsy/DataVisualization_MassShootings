de5535ae890e05ad5e4a856e4060fab640697337
MrWormsy <antonin.martin.rosa@gmail.com>
2020-01-17 21:07:08 +0100

    app.js Added

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.listen(3000);
+console.log("waiting on localhost:3000");
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
