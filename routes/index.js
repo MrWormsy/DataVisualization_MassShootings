dcc8c34963f669a1209961f122e9bfbe16fa5338
MrWormsy <antonin.martin.rosa@gmail.com>
2020-01-20 18:02:22 +0100

    The server will now be running at port 3001

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ let express = require('express');
 let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/mass_shooting_us', function(req, res, next) {
+  res.render('index', {});
 });
 
 module.exports = router;
